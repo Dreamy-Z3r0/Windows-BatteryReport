3a2e270a7c01a221149ad536c0a6d9e62ecadafe
Dreamy-Z3r0 <danielblake98vt@gmail.com>
2023-11-09 14:22:30 +0700

    Update autorun-batteryreport.py

--- autorun-batteryreport.py
+++ autorun-batteryreport.py
@@ -9,6 +9,7 @@ extension = '.html'
 baseCommand = "powercfg /batteryreport /output "
 
 fileName = ''
+subfolder = ''
 fileDirectory = "D:\\battery-report"
 
 report_fullPath = ''
@@ -17,7 +18,7 @@ shellCommand = ''
 
 # Generate file name accordingly to the date and time of report
 def generate_fileName():
-    global fileName     # Format: "\YYYY-MM-DD HH-MM.html"
+    global fileName, subfolder     # Format: "\YYYY-MM-DD HH-MM.html"
 
     # Extract datetime data
     current_datetime = datetime.datetime.now()
@@ -35,24 +36,26 @@ def generate_fileName():
     temp = int(current_datetime.day)
     if (temp < 10):
         fileName += '0'
-    fileName += str(temp) + ' '
+    fileName += str(temp)
+
+    subfolder = fileName
 
     # Fourth field: hour
     temp = int(current_datetime.hour)
     if (temp < 10):
         fileName += '0'
-    fileName += str(temp) + separator
+    fileName += ' ' + str(temp) + separator
 
     # Fifth field: minute
     temp = int(current_datetime.minute)
     if (temp < 10):
         fileName += '0'
-    fileName += str(temp) + extension
+    fileName += str(temp)
 
 #Generate shell command
 def generate_shellCommand():
     global shellCommand    
-    fileDirectory_full = ' "' + fileDirectory + fileName + '"'
+    fileDirectory_full = ' "' + fileDirectory + subfolder + fileName + '"'
     shellCommand = baseCommand + fileDirectory_full
 
 def run_shellCommand():
