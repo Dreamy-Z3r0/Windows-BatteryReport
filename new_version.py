63bfd3daf2138e897a52ebe6cc5491e41d0518b0
Dreamy-Z3r0 <danielblake98vt@gmail.com>
2024-02-22 21:55:21 +0700

    break into modules

--- new_version.py
+++ new_version.py
@@ -1,27 +1,11 @@
-import subprocess
+import update_fullChargedCapacity
 import update_datetime
 
-# Obtain battery full-charged capacity infomation
-systeminfo = subprocess.run(["Powershell", "-NonInteractive", "-Command", "Get-WmiObject", "-Namespace",  "'root\\wmi'", "-Query",  "'select FullChargedCapacity  from BatteryFullChargedCapacity'"], capture_output=True).stdout.decode().strip()
-
-# Look for the line containing the battery full-charged capacity
-data_field_index = systeminfo.index('FullChargedCapacity :')
-end_of_data_line = systeminfo.index('\n', data_field_index)
-
-# Slice string for data line
-systeminfo = systeminfo[data_field_index : end_of_data_line]
-
-# Obtain the data field index within the data line
-data_field_index = systeminfo.index(':') + 2
-
-# Slice data field for data
-systeminfo = systeminfo[data_field_index:]
-
-# Convert data from string to int
-fullcharge_capacity = int(systeminfo)
 
 # Print timestamp
-print(update_datetime.update_datetime())
+timestamp = update_datetime.update_datetime().split(' ')
+print(timestamp)
 
 # Print the obtained data
-print(fullcharge_capacity)
+fullChargedCapacity = update_fullChargedCapacity.update_fullChargedCapacity()
+print(fullChargedCapacity)
