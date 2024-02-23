886197269dca0810af702b3432300bb46e527bad
Dreamy-Z3r0 <34434717+Dreamy-Z3r0@users.noreply.github.com>
2024-02-23 16:08:26 +0700

    Generalise handle_log.py

--- handle_log.py
+++ handle_log.py
@@ -1,33 +1,93 @@
 import os
 
-logFilename = './Monitor_BatteryCapacity.csv'
-logHeaderRow = ['Date', 'Time', 'Full-charged Capacity\n']
+
+###############################
+# Define types of log headers #
+###############################
+
+# Full-charged capacity log file
+logHeaderRow_fullchargedCapacity = ['Date', 'Time', 'Full-charged Capacity\n']
 
 
-def file_check():
-    isExisting = os.path.isfile(logFilename)
 
+############################
+# Define package functions #
+############################
+
+# Get directory components
+def analyse_dir(fileFullPath):
+    container = [0, 0]
+    try:
+        container = fileFullPath.split('\\')
+    except:
+        print(f"{fileFullPath} is not a valid directory.")
+    finally:
+        return container
+
+
+# Check if log file exists
+def file_check(fileFullPath):
+    isExisting = os.path.isfile(fileFullPath)
     if not(isExisting):
-        f = open(logFilename, 'x')
+        # Obtain the directory part
+        logContainer = analyse_dir(fileFullPath)
+        del logContainer[-1]
+        logContainer = '\\'.join(logContainer)      
+
+        # Check if the folder for the log is the working directory
+        if not('.' == logContainer):
+            print(logContainer)
+            # Create the folder if it does not exist
+            if not(os.path.isdir(logContainer)):
+                os.makedirs(logContainer)
+                print("Here")
+
+        # Create the log file if it does not exist
+        f = open(fileFullPath, 'x')
         f.close()
 
     return isExisting
 
 
-def file_write_field(listInput):
-    if (isinstance(listInput, list)):
-        if not(file_check()):
-            file_write_field(logHeaderRow)
+# Main function: write to file
+def file_write_field(fileFullPath, listInput):
+    try:
+        # Analyse log filename
+        logFilename = analyse_dir(fileFullPath)
+        logFilename = logFilename[-1]
 
-        file_newLine = ','.join(listInput)
-        f = open(logFilename, 'a')
-        f.write(file_newLine)
-        f.close()
-    else:
-        print(f"Error: Expected <class 'list'> input.\n\t'{listInput}' is of {type(listInput)}.")
+        if ('Monitor - Battery Full-charged Capacity.csv' == logFilename):
+            logFilename = 1
+        else:
+            raise Exception(f"Error: Invalid log name: {logFilename}")
 
+        if (isinstance(listInput, list)):
+            if not(file_check(fileFullPath)):
+                if (1 == logFilename):
+                    file_write_field(fileFullPath, logHeaderRow_fullchargedCapacity)
+
+            file_newLine = ','.join(listInput)
+            f = open(fileFullPath, 'a')
+            f.write(file_newLine)
+            f.close()
+        else:
+            raise Exception(f"Error: Expected <class 'list'> input.\n\t'{listInput}' is of {type(listInput)}.")
+    except Exception as error_msg:
+        print(error_msg)
+
+
+
+#########################
+# Independent unit test #
+#########################
 
 if __name__ == '__main__':
-    file_write_field(['field 1', 'field 2', 'field 3\n'])
-    file_write_field(['field 4', 'field 5', 'field 6\n'])
-    file_write_field('This is a wrong input type')
+    logContainer = '.'
+    logFilename = 'Monitor - Battery Full-charged Capacity.csv'
+
+    logFilename_fullpath = logContainer + '\\' + logFilename
+
+    file_write_field(logFilename_fullpath, ['field 1', 'field 2', 'field 3\n'])
+    file_write_field(logFilename_fullpath + 'meh', ['field 4', 'field 5', 'field 6\n'])
+    file_write_field(logFilename_fullpath, 'This is a wrong input type')
+    file_write_field(logFilename_fullpath, ['field 4', 'field 5', 'field 6\n'])
